babb8ecbafb793b6c4a49c72cb733386d02461b1
Stephen Wright <sjw948@gmail.com>
2018-02-26 20:15:13 +0000

    dad validation for option types

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -6,7 +6,7 @@ interface Options {
   animation?: string,
   animationSpeed?: number,
   delayActive?: boolean,
-  slide?: Array<Slide>,
+  slides?: Array<Slide>,
 }
 
 interface Slide {
@@ -22,11 +22,78 @@ interface Slide {
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-  @Input() Options: Options;
+  @Input() options: Options;
 
-  constructor() { }
+  constructor(
+    private validators: object,
+    private animations: object,
+  ) {
+    this.validators = {
+      height: this.validateHeight,
+      width: this.validateWidth,
+      animation: this.validateAnimation,
+      animationSpeed: this.validateAnimationSpeed,
+      delayActive: this.validateDelayActive,
+      slides: this.validateSlides,
+    }
+
+    this.animations = {
+      slide: () => null,
+      fade: () => null,
+    }
+  }
 
   ngOnInit() {
+    this.validateOptions();
   }
+
+  validateOptions() {
+    if (!this.options) {
+      throw new Error('Please provide an options configuration object to the carousel component');
+    }
+
+    for (const option of Object.keys(this.options)) {
+      this.validators[option].call()
+    }
+  }
+
+  validateHeight() {
+    if (typeof this.options.height !== 'number') {
+      throw new Error('Height must be a number');
+    }
+  }
+
+  validateWidth() {
+    if (typeof this.options.width !== 'number') {
+      throw new Error('Width must be a number');
+    }
+  }
+
+  validateAnimation() {
+    if (!this.animations[this.options.animation]) {
+      throw new Error('Invalid animation');
+    }
+  }
+
+  validateAnimationSpeed() {
+    if (typeof this.options.animationSpeed !== 'number') {
+      throw new Error('Animation speed must be a number');
+    }
+  }
+
+  validateDelayActive() {
+    if (typeof this.options.delayActive !== 'boolean') {
+      throw new Error('Delay active must be either true or false boolean values');
+    }
+  }
+
+  validateSlides() {
+    if (!Array.isArray(this.options.slides)) {
+      throw new Error('Please provide slides as an array');
+    }
+  }
+
+  // add left 100% per slide in the carousel
+  // at the end of the carousel, find a graceful way of restarting
 
 }
